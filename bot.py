1d834fc6ab3063e42893c039b2acd77cdfef68b2
sage228 <87944907+sage228@users.noreply.github.com>
2022-07-04 09:53:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,7 @@
 import datetime
 import config
-import logging
 
 from aiogram import Bot, Dispatcher, executor, types
-
-#log level
-logging.basicConfig(level=logging.INFO)
 
 #bot init
 bot = Bot(token=config.TOKEN)
