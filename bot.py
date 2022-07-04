6b2ad6c1a0ad0aabc407eff1ebf8513318dc5280
sage228 <87944907+sage228@users.noreply.github.com>
2022-07-04 11:32:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,24 @@ dp = Dispatcher(bot)
 #echo
 @dp.message_handler()
 async def echo(message: types.Message):
-    await message.answer('До дня рождения принцески осталось ')
+    today = datetime.datetime.now()
+    bday = datetime.datetime(2022, 7, 6)
+    st = ''
+    raznitsa = str(bday - today)
+    if raznitsa.find('day')!=-1:
+        day = raznitsa[:raznitsa.find(' ')]
+    if day=='2':
+        st = st+'2 дня'
+    elif day =='1':
+        st = st+'1 день'
+    raznitsa = raznitsa[raznitsa.find(',')+2:]
+    st = st+' ' + raznitsa[:raznitsa.find(':')] + ' часов'
+    raznitsa=raznitsa[raznitsa.find(':')+1:]
+    st = st + ' ' + raznitsa[:raznitsa.find(':')] + ' минут'
+    raznitsa=raznitsa[raznitsa.find(':')+1:]
+    st = st + ' ' + raznitsa[:raznitsa.find('.')] + ' секунд'
+    await message.answer('До дня рождения принцески осталось ' + st)
+
 
 # run long-polling
 if __name__=='__main__':
