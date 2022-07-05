265e97f9e4416e8a7ac833db54125cd10f7cc781
sage228 <87944907+sage228@users.noreply.github.com>
2022-07-05 23:05:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,21 +10,13 @@ dp = Dispatcher(bot)
 #echo
 @dp.message_handler()
 async def echo(message): 
-    delta = datetime.timedelta(hours=3, minutes=0)
-    today = datetime.now(datetime.timezone.utc) + delta
+    today = datetime.datetime.now()
     bday = datetime.datetime(2022, 7, 6)
     st = ''
     raznitsa = str(bday - today)
     if raznitsa.find('day')!=-1:
         day = raznitsa[:raznitsa.find(' ')]
-        if day=='2':
-            st = st+'2 дня'
-        elif day =='1':
-            st = st+'1 день'
-    else:
-        st=st+'0 дней'
     raznitsa = raznitsa[raznitsa.find(',')+2:]
-    st = st+' ' + str(int(raznitsa[:raznitsa.find(':')])) + ' часов'
     raznitsa=raznitsa[raznitsa.find(':')+1:]
     st = st + ' ' + raznitsa[:raznitsa.find(':')] + ' минут'
     raznitsa=raznitsa[raznitsa.find(':')+1:]
